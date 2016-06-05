f156d804098cc9cef59149f583368e063790827a
Haihan Ji <daji@163.com>
2016-06-05 14:49:36 +0800

    Init

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :redmine_mail_list do
+  name 'Redmine Mail List plugin'
+  author 'Haihan Ji'
+  description 'This plugin provides mail list feature for Redmine'
+  version '0.0.1'
+  url 'https://github.com/jihh/redmine_mail_list'
+end
